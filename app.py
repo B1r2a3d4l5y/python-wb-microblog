056d5dbba5b0ce532d3213649ad6cadd0aeec501
B1r2a3d4l5y <82877822+B1r2a3d4l5y@users.noreply.github.com>
2021-08-25 09:56:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ load_dotenv
 
 def create_app():
     app = Flask(__name__)
-    client = MongoClient("mongodb+srv://Bradley:08102000BradleyArnold@microblog-app.ucno1.mongodb.net/test",   ssl=True, ssl_cert_reqs='CERT_NONE')
+    client = MongoClient("mongodb+srv://Bradley:password@microblog-app.ucno1.mongodb.net/test",   ssl=True, ssl_cert_reqs='CERT_NONE')
     app.db = client.microblog
 
     @app.route("/", methods=["GET", "POST"])
@@ -18,7 +18,7 @@ def create_app():
             formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
             app.db.entries.insert( {"content": entry_content, "date": formatted_date})
 
-        entries_with_date = [
+        entries_with_date
             (
                 entry["content"],
                 entry["date"],
