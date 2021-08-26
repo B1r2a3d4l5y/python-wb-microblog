8900bdf570217106e950af0143c9b9c0b6585b9c
Bradley Wayne Arnold <82877822+B1r2a3d4l5y@users.noreply.github.com>
2021-08-26 10:22:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,16 @@ load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    client = MongoClient("mongodb+srv://Bradley:0810200BradleyArnold@microblog-app.ucno1.mongodb.net/test",  ssl=True, ssl_cert_reqs='CERT_NONE')
+    client = MongoClient("mongodb+srv://Bradley:0810200BradleyArnold@microblog-app.ucno1.mongodb.net/test",
+                         ssl=True, ssl_cert_reqs='CERT_NONE')
     app.db = client.microblog
 
     @app.route("/", methods=["GET", "POST"])
     def home():
         if request.method == "POST":
             entry_content = request.form.get("content")
-            entries_with_date = [
+
+    entries_with_date = [
                 (
                     entry["content"],
                     entry["date"],
@@ -24,4 +26,12 @@ def create_app():
                 )
                 for entry in app.db.entries.find({})
             ]
-        return render_template("home.html", entries_with_date)
+
+    return render_template("home.html", entries=entries_with_date)
+
+    return app
+                 
+
+
+
+            
