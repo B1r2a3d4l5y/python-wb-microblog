00c96f17ca4204182bf1d3ee7b03143be0ac2745
Bradley Wayne Arnold <82877822+B1r2a3d4l5y@users.noreply.github.com>
2021-08-27 08:53:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,15 +17,15 @@ def create_app():
             entry_content = request.form.get("content")
             formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
             app.db.entries.insert({"content": entry_content,"date": formatted_date})
-        
-        entries_with_date = [
-            (
-                entry["content"],
-                entry["cdate"],
-                datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
-            )
-            for entry in app.db.entries.find({})
-        ]
-        return render_template("home.html",entries = entries_with_date)
-    return app
+    
+    entries_with_date = [
+        (
+            entry["content"],
+            entry["date"],
+            datetime.datetime.strptime(entry["date","%Y-%m-%d"]).strftime("%b %d")
+        )
+        for entry in app.db.entries.find({})
+    ]
+    return render_template("home.html", entries= entries_with_date)
 
+    return app
