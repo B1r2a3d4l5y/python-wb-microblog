47d7eb3616d6eb061af00c082996fdedf74d58d7
Bradley Wayne Arnold <82877822+B1r2a3d4l5y@users.noreply.github.com>
2021-09-07 15:18:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,24 +8,26 @@ load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    client = MongoClient("mongodb+srv://Bradley:0810200BradleyArnold@microblog-app.ucno1.mongodb.net/test",ssl=True, ssl_cert_reqs='CERT_NONE')
-    app.db = client.microblog
+    client = MongoClient
+    app.db = client.microblog ("mongodb+srv:// Braadley:<BradleyArnold08102000>@microblog-app.net/python-wb-microblog")
 
     @app.route("/", methods=["GET", "POST"])
     def home():
         if request.method == "POST":
             entry_content = request.form.get("content")
             formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-            app.db.entries.insert({"content": entry_content,"date": formatted_date})
-        
+            app.db.entries.insert(
+                {"content": entry_content, "date": formatted_date})
+
         entries_with_date = [
             (
                 entry["content"],
                 entry["date"],
-                datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
+                datetime.datetime.strptime(
+                    entry["date"], "%Y-%m-%d").strftime("%b %d")
             )
             for entry in app.db.entries.find({})
         ]
-        return render_template("home.html",entries = entries_with_date)
+        return render_template("home.html", entries=entries_with_date)
 
     return app
